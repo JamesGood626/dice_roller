9a89bb12c8b919198360291ebe49631cfdca7e2b
JamesGood626 <jamesgood626@gmail.com>
2019-03-25 13:02:42 -0400

    got dice games

--- dice.js
+++ dice.js
@@ -1,51 +1,18 @@
 window.onload = () => {
   const board = document.querySelector(".dice-board");
   const addDiceGameBtn = document.querySelector(".add-dice-game");
-  const diceGameHtmlElements = {
-    htmlType: "div",
-    className: "dice-game-container",
-    children: [
-      { htmlType: "h3", innerText: "Rolled:" },
-      {
-        htmlType: "div",
-        className: "rolled-stats-container",
-        children: [
-          { htmlType: "h4", className: "mean" },
-          { htmlType: "h4", className: "median" },
-          { htmlType: "h4", className: "mode" }
-        ]
-      }
-    ]
-  };
-  setUpAddGameListener(addDiceGameBtn, board, diceGameHtmlElements);
+  setUpAddGameListener(addDiceGameBtn, board, gameFactory());
 };
 
-// About to use this to create dynamic html
-const initializeNewGameHtml = (rolled, mean, median, mode) => ({
-  htmlType: "div",
-  className: "dice-game-container",
-  children: [
-    { htmlType: "h3", innerText: `Rolled: ${rolled}` },
-    {
-      htmlType: "div",
-      className: "rolled-stats-container",
-      children: [
-        { htmlType: "h4", className: "mean", innerHtml: `Mean: ${mean}` },
-        { htmlType: "h4", className: "median", innerHtml: `Median: ${median}` },
-        { htmlType: "h4", className: "mode", innerHtml: `Mode: ${mode}` }
-      ]
-    }
-  ]
-});
-
-const setUpAddGameListener = (btn, board, diceGameHtmlElements) => {
+const setUpAddGameListener = (btn, board, gameFactory) => {
   btn.addEventListener("click", () =>
-    appendGameToBoard(board, diceGameHtmlElements)
+    appendGameToBoard(board, gameFactory.initializeGame())
   );
 };
 
-const appendGameToBoard = (board, diceGameHtmlElements) => {
-  const diceContainer = makeHtmlElements(diceGameHtmlElements);
+const appendGameToBoard = (board, game) => {
+  console.log("THE DICE GAME HTML ELEMENTS: ", game.initializeNewGameHtml);
+  const diceContainer = makeHtmlElements(game.initializeNewGameHtml());
   console.log(diceContainer);
   board.appendChild(diceContainer);
 };
@@ -73,35 +40,189 @@ const iterateAndAppendChildren = (parent, childrenArr) => {
     if (child.hasOwnProperty("children")) {
       htmlInstance = iterateAndAppendChildren(htmlInstance, child.children);
     }
+    if (child.htmlType === "button") {
+      htmlInstance.addEventListener("click", child.onClick);
+    }
     return htmlInstance;
   });
   childrenHtmlElements.forEach(child => parent.appendChild(child));
   return parent;
 };
 
-function RollTrack(rolls, rollCount) {
+const gameFactory = () => {
+  let count = 0;
   return {
-    rolls: rolls,
-    rollCount: rollCount,
-    diceRoll: function() {
-      return Math.ceil(Math.random() * 6) * 2;
+    initializeGame: function() {
+      const gameId = count;
+      const config = {
+        gameId,
+        rollCallback: this.reRenderCurrentGame,
+        rolls: [],
+        rollCount: 0,
+        mean: 0,
+        median: 0,
+        mode: 0
+      };
+      count++;
+      return new RollTracker(config);
     },
-    rollIt: function() {
-      this.rollCount++;
-      this.rolls.push(this.diceRoll());
-      return this.newRollTracker(this.rolls, this.rollCount);
-    },
-    newRollTracker: function(rolls, rollCount) {
-      return new RollTrack(rolls, rollCount);
+    reRenderCurrentGame: function(gameId, updatedDiceGame) {
+      // grab game-${gameId}
+      const diceGameHtml = document.querySelector(`.game-${gameId}`);
+      console.log("RE RENDERING: ", updatedDiceGame);
+      const { rolls, mean, median, mode } = updatedDiceGame;
+      // The h3
+      diceGameHtml.childNodes[0].textContent = `Rolled: ${rolls}`;
+      // The stats div
+      const [
+        meanHtmlEl,
+        medianHtmlEl,
+        modeHtmlEl
+      ] = diceGameHtml.childNodes[1].childNodes;
+      meanHtmlEl.textContent = mean;
+      medianHtmlEl.textContent = median;
+      modeHtmlEl.textContent = mode;
     }
   };
-}
+};
 
-const mean = (rolls, count) =>
-  rolls.reduce((acc, num) => {
-    acc = acc + num;
-    return acc;
-  }, 0) / count;
+class RollTracker {
+  constructor(config) {
+    const {
+      gameId,
+      rollCallback,
+      rolls,
+      rollCount,
+      mean,
+      median,
+      mode
+    } = config;
+    console.log("THE ROLL CALLBACK: ", rollCallback);
+    this.gameId = gameId;
+    this.rollCallback = rollCallback;
+    this.rolls = rolls;
+    this.rollCount = rollCount;
+    this.mean = mean;
+    this.median = median;
+    this.mode = mode;
+  }
+
+  diceRoll() {
+    return Math.ceil(Math.random() * 6) * 2;
+  }
+
+  rollIt() {
+    this.rollCount++;
+    this.rolls.push(this.diceRoll());
+    const { mean, median, mode } = this.calculateStatsOnRoll();
+    let config = {
+      gameId: this.gameId,
+      rollCallback: this.rollCallback,
+      rolls: this.rolls,
+      rollCount: this.rollCount,
+      mean: mean,
+      median: median,
+      mode: mode
+    };
+    return new RollTracker(config);
+  }
+
+  calculateMean(rolls, count) {
+    const sum = rolls.reduce((acc, num) => {
+      acc = acc + num;
+      return acc;
+    }, 0);
+    return sum / count;
+  }
+
+  calculateMedian(rolls, count) {
+    const leastToGreatest = (a, b) => a - b;
+    const mid = Math.floor(count / 2);
+    return rolls.sort(leastToGreatest)[mid];
+  }
+
+  calculateMode(rolls) {
+    const { trackMap, uniqueNums } = rolls.reduce(
+      (acc, num) => {
+        if (!acc.trackMap[num]) {
+          acc.trackMap[num] = 1;
+          acc.uniqueNums.push(num);
+        } else {
+          acc.trackMap[num]++;
+        }
+        return acc;
+      },
+      { trackMap: {}, uniqueNums: [] }
+    );
+    const { mode } = uniqueNums.reduce(
+      (acc, num) => {
+        // Facilities bimodal/trimodal/quadrimodal
+        if (trackMap[num] === acc.amount) {
+          acc.mode.push(num);
+        }
+        if (trackMap[num] > acc.amount) {
+          acc.mode = [];
+          acc.mode.push(num);
+          acc.amount = trackMap[num];
+        }
+        return acc;
+      },
+      { mode: [], amount: 0 }
+    );
+    return mode;
+  }
+
+  calculateStatsOnRoll() {
+    const mean = this.calculateMean(this.rolls, this.rollCount);
+    const median = this.calculateMedian(this.rolls, this.rollCount);
+    const mode = this.calculateMode(this.rolls);
+    return { mean, median, mode };
+  }
+
+  reRenderUpdatedGameHtml() {
+    const gameAfterRoll = this.rollIt();
+    this.rollCallback(this.gameId, gameAfterRoll);
+  }
+
+  initializeNewGameHtml() {
+    return {
+      htmlType: "div",
+      className: `dice-game-container game-${this.gameId}`,
+      children: [
+        { htmlType: "h3", innerText: `Rolled: ${this.rolls.toString()}` },
+        {
+          htmlType: "div",
+          className: "rolled-stats-container",
+          children: [
+            {
+              htmlType: "h4",
+              className: "mean",
+              innerText: `Mean: ${this.mean}`
+            },
+            {
+              htmlType: "h4",
+              className: "median",
+              innerText: `Median: ${this.median}`
+            },
+            {
+              htmlType: "h4",
+              className: "mode",
+              innerText: `Mode: ${this.mode.toString()}`
+            }
+          ]
+        },
+        {
+          htmlType: "button",
+          className: "roll-btn",
+          innerText: "Roll!",
+          // removing the click event listener on this
+          // element on rerender is necessary.
+          onClick: this.reRenderUpdatedGameHtml.bind(this)
+        }
+      ]
+    };
+  }
+}
 
 // Straight from MDN:
 //   To compare numbers instead of strings, the compare function can simply subtract b from a.
@@ -109,24 +230,3 @@ const mean = (rolls, count) =>
 const leastToGreatest = (a, b) => a - b;
 // I think there's more to picking a median than just this, but it'll do for now.
 const median = (rolls, count) => rolls.sort(leastToGreatest)[count / 2];
-
-const tracker = new RollTrack([], 0);
-// console.log("tracker: ", tracker);
-const tracker2 = tracker.rollIt();
-// console.log("tracker2: ", tracker2);
-const tracker3 = tracker2.rollIt();
-// console.log("tracker3: ", tracker3);
-
-// console.log(tracker === tracker2);
-// console.log(tracker2 === tracker3);
-
-const tracker4 = tracker3.rollIt();
-const tracker5 = tracker4.rollIt();
-const tracker6 = tracker5.rollIt();
-const tracker7 = tracker6.rollIt();
-
-console.log("THE ROLLS: ", tracker7.rolls);
-console.log(mean(tracker7.rolls, tracker7.rollCount));
-console.log("THE MEDIAN: ");
-console.log(tracker7.rolls.sort(leastToGreatest));
-console.log(median(tracker7.rolls, tracker7.rollCount));
